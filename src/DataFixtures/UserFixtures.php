5bfb5f1e33a553e30f90f9cbc8464aa4a4ba54f3
Karim <abdounikarim@gmail.com>
2018-08-06 13:54:11 +0200

    add admin link in header and update user fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,15 +5,27 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $user = new User();
         $user->setUsername('Jean Forteroche');
         $user->setEmail('jf@alaska.fr');
-        $user->setPassword('alaska');
+        $password = $this->encoder->encodePassword($user, 'alaska');
+        $user->setPassword($password);
         $roles = ['ROLE_ADMIN'];
         $user->setRoles($roles);
 
