615294af4512a03e6f82852b1e30c410c76da68c
Karim <abdounikarim@gmail.com>
2018-09-07 10:14:00 +0200

    customize user form type

---
+++ src/Form/UserType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email'
+            ])
+            ->add('username', TextType::class, [
+                'label' => 'Pseudo'
+            ])
+            ->add('image', FileType::class, [
+                'label' => 'Image'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
